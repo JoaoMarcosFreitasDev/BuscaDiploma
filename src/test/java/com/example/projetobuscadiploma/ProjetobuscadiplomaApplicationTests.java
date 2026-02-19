098cbca110c51565b6c5ef53bb12e42083ed3c17
JoaoMarcosFreitasDev <joaomarcosfreitasdev@gmail.com>
2026-02-18 22:17:31 -0300

    Subindo repositório do projeto

---
+++ src/test/java/com/example/projetobuscadiploma/ProjetobuscadiplomaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projetobuscadiploma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetobuscadiplomaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
